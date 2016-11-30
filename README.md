64f37ad882d9348efc62bba3efa389c4ac05e84f
Yash Chandra <yash.chandra@gmail.com>
2016-11-30 15:59:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# jslearner
+This is an exercise to practice and learn Plain Javascript. No Frameworks and not even JQuery. I want to see if I can build small components using vanilla/plain js only. 
+
+## Browser Support
+
+IE 9+ (sorry IE8 or below. No time for you anymore)
+
+Chrome (Latest versions)
+
+Firefox (Latest Versions)
+
+Safari (At your own peril :)
+
+## Projects
+
+- typeahead: build a typeahead/search in plain js
+
---
+++ typeahead/typeahead.js
@@ -0,0 +1,50 @@
+function showSearchValues(input) {
+ var filter, table, tr, td, i;
+ //input = document.getElementById("myInput");
+if(input.value == "") {
+document.getElementById("myTable").style.display="none";
+} else {
+document.getElementById("myTable").style.display="block";
+}
+//alert(input.value)
+ 
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "block";
+       
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+function startSearch(e,myInput,myTable) {
+   
+	document.getElementById("myInput").value=e.target.textContent;
+    document.getElementById("myTable").style.display="none";
+    
+    //fire actual search
+    console.log('Searching for'+ e.target.textContent)
+      console.log(e.target)
+
+}
+
+function buildTypeaheadData(data,myTable) {
+
+
+      for(i=0;i<data.length;i++) {
+   var node = document.createElement("tr");
+   var node2 = document.createElement("td");
+var textnode = document.createTextNode(data[i]);  
+node.appendChild(node2)
+node2.appendChild(textnode);                            
+myTable.appendChild(node);   
+   }
+}
+
+
