99328059e8f0fb4cfdfba96a8060e87bf795e5b1
Yash Chandra <yash.chandra@gmail.com>
2016-11-30 17:17:55 -0500

    fixes

--- typeahead/typeahead.js
+++ typeahead/typeahead.js
@@ -1,48 +1,40 @@
-function showSearchValues(input) {
- var filter, table, tr, td, i;
- //input = document.getElementById("myInput");
-if(input.value == "") {
-document.getElementById("myTable").style.display="none";
-} else {
-document.getElementById("myTable").style.display="block";
-}
-//alert(input.value)
- 
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "block";
-       
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
-function startSearch(e,myInput,myTable) {
-   
-	myInput.value=e.target.textContent;
-    myTable.style.display="none";
-    
-    //fire actual search
 
-}
+function showSearchValues(input,myTable) {
+    var filter, table, tr, td, i;
+    if(input.value == "") {
+        myTable.style.display="none";
+    } else {
+        myTable.style.display="block";
+    }
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "block";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+    }
+}// End function showSearchValues
+
+function startSearch(e,myInput,myTable) {
+	  myInput.value=e.target.textContent;
+    myTable.style.display="none";
+}// End function startSearch
 
 function buildTypeaheadData(data,myTable) {
-
-
-      for(i=0;i<data.length;i++) {
-   var node = document.createElement("tr");
-   var node2 = document.createElement("td");
-var textnode = document.createTextNode(data[i]);  
-node.appendChild(node2)
-node2.appendChild(textnode);                            
-myTable.appendChild(node);   
+    for(i=0;i<data.length;i++) {
+        var node = document.createElement("tr");
+        var node2 = document.createElement("td");
+        var textnode = document.createTextNode(data[i]);  
+        node.appendChild(node2)
+        node2.appendChild(textnode);                            
+        myTable.appendChild(node);   
    }
-}
+}// End function buildTypeaheadData
 
 
